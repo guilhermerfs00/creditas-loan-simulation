8afb2992dd1549acec084147fd045cf7336e745d
guilhermerfs00 <guilherme.rfs00@gmail.com>
2025-07-15 17:03:14 -0300

    Initial commit

---
+++ ce-auth-ms/src/main/java/com/creditas/ce_auth_ms/CeAuthMsApplication.java
@@ -0,0 +1,13 @@
+package com.creditas.ce_auth_ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CeAuthMsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CeAuthMsApplication.class, args);
+	}
+
+}
---
+++ ce-auth-ms/src/test/java/com/creditas/ce_auth_ms/CeAuthMsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.creditas.ce_auth_ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CeAuthMsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
